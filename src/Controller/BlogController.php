9276399d63edc3e84b25fec370c7f2d81ad8e188
Vitaliy Poletaev <hapdev22@gmail.com>
2022-04-11 00:27:48 +0700

    Task-2 | Created app fixtures and added pagination

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
-    public function index(): Response
+    /**
+     * @throws \Exception
+     */
+    public function index(Request $request, int $page, PostRepository $posts): Response
     {
+        $sortMethod = $request->get("method");
+        $latestPosts = $posts->findLatest($page, empty($sortMethod) ? "DESC" : $sortMethod);
         return $this->render('blog/index.html.twig', [
             'title' => 'Blog index page',
+            'paginator' => $latestPosts,
         ]);
     }
 }
